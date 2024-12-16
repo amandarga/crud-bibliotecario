d121603092949a1059245e34a70c0c8a7370431a
Leonardojscosta1 <118093377+amandarga@users.noreply.github.com>
2024-12-15 23:20:35 -0300

    Create e alguns Inserts iniciais do banco de dados

---
+++ banco_de_dados/Inserts CDD Database.sql
@@ -0,0 +1,4 @@
+INSERT INTO cdd (codigo, descricao, area) VALUES
+('000', 'Computadores e Informação', 'Ciência'),
+('100', 'Filosofia e Psicologia', 'Humanas'),
+('500', 'Ciências Naturais e Matemática', 'Exatas');
